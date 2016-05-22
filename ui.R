e835d38550a8c8d4c547b802f1e95d64614eb1a5
Milan Shrestha <melawn@gmail.com>
2016-05-23 00:50:30 +0545

    few changes in ui.R file

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI( pageWithSidebar(
     headerPanel("Interest Calulator"),
     sidebarPanel(
         textInput('principal', 'Principal', value = ""),
-        textInput('rate', 'Interest Rate', value = ""),
+        textInput('rate', 'Interest Rate (R/100,ie .12 is 12%)', value = ""),
         textInput('years', 'years', value = ""),
         actionButton("goButton", "Go!")
     ), 
@@ -20,7 +20,7 @@ shinyUI( pageWithSidebar(
         h4('Your new Amount is:'),
         verbatimTextOutput("outputValue"),
         h5('Please note that Go button is requied to be clicked only Once.Values after that can be direclty changed in the textbox and using the Reactivity feature of shiny calulations is done immediatly and resuts are displayed.'),
-        "Source Code of the application is available at", a("https://github.com/",href="https://github.com/")
+        "Source Code of the application is available ", a("here",href="https://github.com/milanshrestha77/shiny-application")
         )
         )
 )
